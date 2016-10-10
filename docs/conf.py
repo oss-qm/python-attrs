c8973ab31a2804dc30763252def050803a59ae33
Tristan Seligmann <mithrandi@mithrandi.net>
2016-10-10 04:40:30 +0200

    Point intersphinx_mapping at local Python docs.

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ texinfo_documents = [
 ]
 
 intersphinx_mapping = {
-    "https://docs.python.org/3": None,
+    'python': ('/usr/share/doc/python3-doc/html', '/usr/share/doc/python3-doc/html/objects.inv')
 }
 
 # Allow non-local URIs so we can have images in CHANGELOG etc.
